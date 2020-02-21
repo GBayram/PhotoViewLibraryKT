c9ab68a0e743b8c9c050b8a5f2e8dec5872afcae
Gokmen Bayram <bayramgokmenn@gmail.com>
2020-02-21 12:59:19 +0300

    completed.

--- app/src/main/java/com/library/photoview/MainActivity.kt
+++ app/src/main/java/com/library/photoview/MainActivity.kt
@@ -2,11 +2,33 @@ package com.library.photoview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import com.github.chrisbanes.photoview.PhotoView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var imageView : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        imageView.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View?) {
+
+        if(v?.id == imageView.id){
+            var mBuilder = AlertDialog.Builder(this)
+            var mView = layoutInflater.inflate(R.layout.custom_dialog_layout,null)
+            var photoView = mView.findViewById<PhotoView>(R.id.photoLibImageView)
+            photoView.setImageResource(R.drawable.flower)
+            mBuilder.setView(mView)
+            var mDialog = mBuilder.create()
+            mDialog.show()
+        }
     }
 }
